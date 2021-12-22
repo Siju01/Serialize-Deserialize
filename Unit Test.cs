f8060c884b82387a5f0eeb7757db1e3dee478ab4
Siju <63763376+Siju01@users.noreply.github.com>
2021-12-22 09:03:50 +0700

    Add files via upload

---
+++ Unit Test.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Serializer;
+using Deserializer;
+using System.IO;
+public class TestClass
+{
+	SerializerApp serializerApp = new SerializerApp();
+	DeserializerApp deserializerApp = new DeserializerApp();
+
+	//tests serialize
+	[Fact]
+	public void SerializeTest()
+	{
+		Assert.Equal(1, serializerApp.Serialize());
+	}
+
+	//tests deserialize
+	[Fact]
+	public void DeserializeTest()
+	{
+		Assert.Equal(1, deserializerApp.Deserialize());
+	}
+
+}
